ecd6dbbe7a4ae8906573e013f66e170a041ff522
Afonso Baldo <up202004598@edu.fe.up.pt>
2022-04-26 19:00:30 +0100

    Create index.php and move header and fotter to essentials.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('template/essentials.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/position.css">
+        <title>Du'Campu</title>
+    </head>
+    <body>
+
+    <?show_header_menu();?>
+
+    <section class="mainPage">
+        <img src = "images/DuCampua.jpg">
+    </section>
+
+    <?show_footer();?>
+    </body>
+</html>
