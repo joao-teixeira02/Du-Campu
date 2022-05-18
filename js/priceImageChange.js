b7512f566298f9fab8d91b2fe8719458c3d146fb
inescardos <ines.caardoso@gmail.com>
2022-05-19 00:55:34 +0100

    icons of restaurants buttons

---
+++ js/priceImageChange.js
@@ -0,0 +1,26 @@
+function addEventAsPriceImageChange(id_box, img_hoover, img_out, img_click){
+    const box = document.querySelector(id_box)
+
+    
+    box.addEventListener('click', (e) => {
+        const this_box= document.getElementById(e.srcElement.id)
+        this_box.toggleAttribute('isSelected')
+        if(this_box.hasAttribute('isSelected'))
+            this_box.src = img_click;
+        else
+            this_box.src = img_out;
+    })
+
+    box.addEventListener('mouseover', () => {
+        if(!box.hasAttribute('isSelected'))
+            box.src = img_hoover;
+        })
+    box.addEventListener('mouseout', () => {
+        if(!box.hasAttribute('isSelected'))
+            box.src = img_out;
+        })
+  }
+
+  addEventAsPriceImageChange('#euro', 'images/euro2.png', 'images/euro.png', 'images/euro3.png')
+  addEventAsPriceImageChange('#doiseuro', 'images/2euro2.png', 'images/2euro.png', 'images/2euro3.png')
+  addEventAsPriceImageChange('#treseuro', 'images/3euro2.png', 'images/3euro.png', 'images/3euro3.png')
