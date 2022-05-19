7d9bd00c60f2c91172e9dde966270d36a25aed78
Afonso Baldo <up202004598@edu.fe.up.pt>
2022-05-19 23:31:03 +0100

    Empty cart is done!

--- js/cart.js
+++ js/cart.js
@@ -1,41 +1,59 @@
 
 const cart_box = document.querySelector("#cart_box");
+var desaparecerCartTimerON = false
+var aparecerCartTimerON = false
+var id = 0
 
 function close_cart(){
+    if(desaparecerCartTimerON )
+        return
     console.log(cart_box.hasAttribute("display_block"))
 
-    cart_box.style.opacity = 1;
-    opacity_value = 1.0;
+    if(aparecerCartTimerON)  
+        clearInterval(id);
 
-    id = setInterval(desaparecer, 1);
+    cart_box.style.opacity = 1;
+    let opacity_value = 1.0;
+    
+    id = window.setInterval(desaparecer, 10);
+    desaparecerCartTimerON = true;
     function desaparecer() {
+        console.log(cart_box.style.opacity )
         if (cart_box.style.opacity == 0) {
-            clearInterval(id);
-            
             cart_box.removeAttribute("display_block");
+            clearInterval(id);
+            desaparecerCartTimerON = false;
         } else {
             opacity_value-=0.1
-            cart_box.style.opacity = opacity_value;
+            cart_box.style.opacity = opacity_value.toFixed(1)
         }
     }
 } 
 
 function show_cart(){
+    if(aparecerCartTimerON)  
+        return
     
+    if(desaparecerCartTimerON)  
+        clearInterval(id);
+
     cart_box.style.opacity = 0;
     
     cart_box.setAttribute("display_block", "");
     opacity_value = 0;
     
-    id = setInterval(aparecer, 1);
+    id = window.setInterval(aparecer, 10);
+    aparecerCartTimerON = true;
     function aparecer() {
-        if (cart_box.style.opacity == 1) {
-            clearInterval(id);
-            
-        } else {
+        console.log(cart_box.style.opacity )
+        if (cart_box.style.opacity != 1) {
             opacity_value+=0.1
             
-            cart_box.style.opacity = opacity_value;
+            cart_box.style.opacity = opacity_value.toFixed(1);
+            setTimeout(aparecer, 0.1)
+        }else{
+            aparecerCartTimerON = false;
+            clearInterval(id);
         }
     }
 
--- template/cart.tpl.php
+++ template/cart.tpl.php
@@ -6,8 +6,16 @@
         <article id="cart_box">
             <header>
             <img clickable class="cross" src="images/close.png" onclick="close_cart()"/>
-
             </header>
+
+            <main>
+                <div id="empty_cart">
+                    <img id = "empty_cart_image" src = "images/cart2.png" width="100px" height="100px" alt="empty cart">
+                    <p>Add items from a restaurant to start a new cart<p>
+                </div>
+                
+
+            </main>
 
 
         </article>
