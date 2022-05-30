39b2dd4d5d167a1406d4c672e631d8793624aac4
joao.teixeira2924@gmail.com <joao.teixeira2924@gmail.com>
2022-05-30 14:23:42 +0100

    Fixing directories

--- utils/session.php
+++ utils/session.php
@@ -1,7 +1,7 @@
 <?php
     declare(strict_types = 1);
 
-    require_once(__DIR__ . "/../cart.class.php");
+    require_once(__DIR__ . "/../database/cart.class.php");
 
     class Session{
         public Cart $cart;
